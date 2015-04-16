f4d99fcce5d7518bed25e35036918766fd77b35c
azmuth94 <dannyaksenov@gmail.com>
2015-04-16 10:32:19 -0400

    initial models and migrations

---
+++ db/migrate/20150416142111_create_items.rb
@@ -0,0 +1,9 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :name 
+      t.decimal :price, precision: 8, scale: 2
+      t.timestamps null: false
+    end
+  end
+end
