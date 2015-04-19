03ba51676d44c6329b941ed392e88114370dd25d
lezoudali <lezou.dali@gmail.com>
2015-04-19 17:21:20 -0400

    filtered articles

--- app/models/concerns/findable.rb
+++ app/models/concerns/findable.rb
@@ -4,7 +4,7 @@ module Findable
   end
 
   def find_articles
-    uri = URI.parse("#{times_base_uri}.json?q=#{symbol}&sort=newest&fq=news_desk:(business)&fl=web_url,snippet&begin_date=20130601&api-key=#{ENV['TIMES_ARTICLE_KEY']}")
+    uri = URI.parse("#{times_base_uri}.json?q=#{company}&sort=newest&fq=news_desk:(business)&fl=web_url,snippet&begin_date=20130601&api-key=#{ENV['TIMES_ARTICLE_KEY']}")
     resp = JSON Net::HTTP.get(uri)
     resp['response']['docs']
   end
