6f8c402a3c1cc290ee194c9fc6c885c97d0d6b15
myklenero <mykle.nero@gmail.com>
2015-04-20 11:12:37 -0400

    Follow and unfollow button

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,5 @@
 class StocksController < ApplicationController
+  include StocksHelper
 
   def index
     @stocks = Stock.all
@@ -6,9 +7,12 @@ class StocksController < ApplicationController
 
   def update
     stock = Stock.find_by_id(params[:stock][:id])
-    unless stock.users.include?(current_user)
+    if params[:follow] == "true"
       stock.users << current_user unless stock.users.include?(current_user)
       flash[:success] = "You are now following #{stock.company}"
+    elsif params[:follow] == "false"
+      stock.users.delete(current_user)
+      flash[:success] = "You stopped following #{stock.company}"
     end
     redirect_to :back
   end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,10 @@
 class Stock < ActiveRecord::Base
   include Findable
-
   has_many :user_stocks
   has_many :users, through: :user_stocks
+
+  def followed_by?(user)
+    user.stocks.include?(self)
+  end
+
 end
