aef65183f228e8cb3d5dbb8d028f8f83e3db4708
lezoudali <lezou.dali@gmail.com>
2015-04-23 14:50:01 -0400

    hmm

--- app/models/concerns/sentiment_analysis.rb
+++ app/models/concerns/sentiment_analysis.rb
@@ -1,4 +1,7 @@
 module SentimentAnalysis
+  @@positive = []
+  @@negative = []
+
   def analyze
     analyzer = SentimentLib::Analyzer.new
     stock_tweets = self.find_tweets(100)
@@ -7,17 +10,27 @@ module SentimentAnalysis
   end
 
   def scaler(array)
-    positive = []
-    neutral = []
-    negative = []
     array.each do |rating|
       if rating >= 1
-        positive << rating
-      elsif rating == 0 
-        neutral << rating
+        @@positive << rating
       else rating <= -1
-        negative << rating
+        @@negative << rating
       end
     end
+    # binding.pry
+  end
+
+  def sum 
+    self.analyze
+   @@positive.count + @@negative.count
+   # binding.pry
+  end
+
+  def positive_percentage
+    @@positive.count / sum * 100
+  end
+
+  def negative_percentage
+    @@negative.count / sum * 100
   end
 end
