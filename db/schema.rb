f6eb56e4c7ff02e935091a2d3fbaa9121ad587b6
myklenero <mykle.nero@gmail.com>
2015-04-24 14:59:56 -0400

    Styled the feed

--- db/schema.rb
+++ db/schema.rb
@@ -104,12 +104,10 @@ ActiveRecord::Schema.define(version: 20150424152205) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
-    t.integer  "list_id"
     t.datetime "deleted_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["list_id"], name: "index_users_on_list_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
