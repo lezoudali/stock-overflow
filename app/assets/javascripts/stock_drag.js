b4803469e03a7cdc730b77f8660c7485cb4495e9
myklenero <mykle.nero@gmail.com>
2015-04-27 13:27:33 -0400

    Drag stocks into favorites (ui)

--- app/assets/javascripts/stock_drag.js
+++ app/assets/javascripts/stock_drag.js
@@ -1,5 +1,23 @@
 $(function(){
-  $("#stock-list").sortable({
+  var draggedFromStocks;
+
+  $("#favorite-stocks").sortable({
     axis: "y"
+  });
+
+  $("#stock-list li").draggable({
+    axis: "y",
+    connectToSortable: "#favorite-stocks"
+  });
+
+  $( "#stock-list li" ).on( "dragstart", function( event, ui ) {
+    draggedFromStocks = true;
+  });
+
+  $("#favorite-stocks").on( "sortreceive", function( event, ui ) {
+    if(draggedFromStocks) {
+      $("#favorite-stocks li").last().remove();
+    }
+    draggedFromStocks = false;
   });
 });
