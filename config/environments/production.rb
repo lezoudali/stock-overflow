927a2a1213556685159592339ef12c7f7c415b8d
lezoudali <lezou.dali@gmail.com>
2015-04-30 18:14:44 -0400

    setting up mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.active_job.queue_adapter = :sucker_punch
+  config.action_mailer.default_url_options = { host: 'stockoverflow.herokuapp.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    authentication:       :plain, 
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    domain:               'heroku.com', 
+    enable_starttls_auto: true
+  }
 end
